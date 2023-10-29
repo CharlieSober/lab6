de84396f927c3a4dee72cfc6861ec56fca24ec32
CharlieSober <cjsober25@gmail.com>
2023-10-29 10:30:52 -0400

    2nd commit

--- lab6.py
+++ lab6.py
@@ -1,7 +1,6 @@
 #Written by Charlie Sober
 
-
-def encoder(password):
+def encode(password):
     password_str = str(password)
     result = ""
     for char in password_str:
@@ -9,5 +8,20 @@ def encoder(password):
         result += str(new_digit)
     return result
 
-org_psswrd = input("Please enter your password: ")
-print(encoder(org_psswrd))
+
+continue_app = True
+
+
+while continue_app:
+    print("Menu")
+    print("-------------")
+    print("1. Encode")
+    print("3. Quit")
+    print()
+    option = int(input("Please enter an option: "))
+    if option == 1:
+        og_psswrd = input("Please enter your password to encode: ")
+        print("Your password has been encoded and stored")
+        encoded_psswrd = encode(og_psswrd)
+    if option == 3:
+        continue_app = False
